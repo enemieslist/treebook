2f47ad1e5794dbd9848690cff21cfe1daec6410b
Daniel Barrett <dan@twofriendlynerds.com>
2013-10-19 13:07:34 -0400

    trying to initialize again, for the first time

---
+++ treebook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '55878b1cc8cf175174bf7a29e00b887ca9b4aff10ddedf6c49935df24712054a4ad066b35529b0af4ebe55eb1434538ccf35518fa204fc08cc791d423e591168'
